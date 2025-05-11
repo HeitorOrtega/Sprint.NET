9399e0c5c7148413adfe5821be8bc219ee177725
HeitorOrtega <rm557825@fiap.com.br>
2025-05-11 19:46:15 -0300

    Inserindo o DTO de moto

---
+++ Sprint_1/DTO/MotoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Sprint_1.DTO;
+
+public class MotoDTO
+{
+    public string Cor { get; set; }
+    public string Placa { get; set; }
+    public DateTime DataFabricacao { get; set; }
+    public long ChaveiroId { get; set; }
+    public List<long> PatioIds { get; set; }
+}
