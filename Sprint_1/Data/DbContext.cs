ad9b1d5b85cc2dd2dd7500c3cbaa798ac4d17583
HeitorOrtega <rm557825@fiap.com.br>
2025-05-11 19:53:30 -0300

    Inserindo o package e a classe para conexao Oracle

---
+++ Sprint_1/Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sprint_1.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Moto> Motos { get; set; }
+    public DbSet<Chaveiro> Chaveiros { get; set; }
+    public DbSet<Patio> Patios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
