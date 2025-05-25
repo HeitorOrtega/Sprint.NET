2c48169059e0c2cbae0b68272d0bc30d62c69180
HeitorOrtega <rm557825@fiap.com.br>
2025-05-25 16:21:15 -0300

    inserindo alteracoes da conexao oracle

--- Sprint_1/Program.cs
+++ Sprint_1/Program.cs
@@ -1,17 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using Sprint_1.Data;
 using Oracle.EntityFrameworkCore;
-using System; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseOracle(Environment.GetEnvironmentVariable("ORACLE_CONNECTION")));
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
 
 var app = builder.Build();
 
